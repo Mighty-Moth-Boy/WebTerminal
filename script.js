704042a3baa999b80825fb54b22aef5fe736322f
Austin Heggen <austin.heggen@outlook.com>
2025-09-22 16:05:25 +1000

    Added new easter egg - testing javascript! Try to figure out how to acess it. Please note: no checking source code please

---
+++ script.js
@@ -0,0 +1,8 @@
+function JSTest() {
+    alert("Press OK to test the JavaScript.")
+    let userInput = prompt("Please enter your text to test JS:");
+    alert("When you press OK, your response will be submitted to the console.")
+    alert("To open your console, you'd normally press F12. Now press OK.")
+    console.log("You entered: " + userInput)
+    console.log("If you see your text, your JavaScript works!")
+}
